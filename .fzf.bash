b5641383e3bab3ed5e35a34b941185b550c6e2a0
Aura <aura8897@gmail.com>
2021-07-25 12:14:08 +0800

    comment function s

--- .fzf.bash
+++ .fzf.bash
@@ -1,6 +1,6 @@
 [[ ! $- =~ i ]] && return
 
-s(){ f=$(rg --color always "$@" | fzf --ansi -m -d: $FZF_PREVIEW_OPTS 'let s={2}-3; [ $s -lt 0 ] && s=1; let e={2}+3; bat --color=always -nr $s:$e -H {2} {1}' | cut -d: -f1); [[ -n $f ]] && vi -- $f; }
+# s(){ f=$(rg --color always "$@" | fzf --ansi -m -d: $FZF_PREVIEW_OPTS 'let s={2}-3; [ $s -lt 0 ] && s=1; let e={2}+3; bat --color=always -nr $s:$e -H {2} {1}' | cut -d: -f1); [[ -n $f ]] && vi -- $f; }
 
 d(){ diff -raq "$@" | fzf -m --bind "ctrl-o:execute(vi {4} -c 'vert diffs {2}' < /dev/tty > /dev/tty)" | sed -nE 's,^Only in (.*): (.*)$,\1/\2,p; s,^Files (.*) and (.*) differ$,\1 \2,p' | sed 's,//,/,'; }
 
