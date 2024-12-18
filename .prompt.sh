ab6562b8da9d8cfbea670164998bff5189609eb8
Aura <aura8897@gmail.com>
2024-12-18 17:39:06 +0800

    bash: upgrade .prompt.sh

--- .prompt.sh
+++ .prompt.sh
@@ -23,68 +23,31 @@ function __promptline_cwd {
 		# pop off last part of cwd
 		local part="${cwd##*/}"
 		cwd="${cwd%/*}"
-
+		let ps1_len+=${#part}+3
 		formatted_cwd="$dir_sep$part$formatted_cwd"
 		part_count=$((part_count+1))
 
 		[[ $part_count -eq $dir_limit ]] && first_char="$truncation" && break
 	done
-
-	printf "%s" "$first_char$formatted_cwd"
+	let ps1_len+=5
+	ps1="${1}$first_char$formatted_cwd${2}"
 }
 
 function jobsnum {
 	local num=`jobs -p | wc -l`
 	jobs | grep '_zlua --add' &> /dev/null && let num--
 	[[ $num -gt 0 ]] || return 1
-	echo -n "${num}"
+	let ps1_len+=${#num}+3
+	ps1="$ps1${1}${num}${2}"
 }
 
 function git_branch {
 	local sha=`git rev-parse --short HEAD 2> /dev/null`
 	[ $sha ] || return 1
 	local branch=`git symbolic-ref --short -q HEAD || git describe --tags --exact-match HEAD 2> /dev/null`
-	[ $branch ] || branch=$sha
-	echo -n " ${branch}"
-}
-
-function __promptline_ps1 {
-	local slice_prefix slice_empty_prefix slice_suffix is_prompt_empty=1
-
-	# section "c" header
-	slice_prefix="${c_bg}${sep}${c_fg}${c_bg}${space}"
-	slice_suffix="$space${c_sep_fg}"
-	slice_empty_prefix="${c_fg}${c_bg}${space}"
-	[ $is_prompt_empty -eq 1 ] && slice_prefix="$slice_empty_prefix"
-	# section "c" slices
-	__promptline_wrapper "$(__promptline_cwd)" "$slice_prefix" "$slice_suffix" && is_prompt_empty=0
-
-	# section "y" header
-	slice_prefix="${y_bg}${sep}${y_fg}${y_bg}${space}"
-	slice_suffix="$space${y_sep_fg}"
-	slice_empty_prefix="${y_fg}${y_bg}${space}"
-	[ $is_prompt_empty -eq 1 ] && slice_prefix="$slice_empty_prefix"
-	# section "y" slices
-	__promptline_wrapper "$(git_branch)" "$slice_prefix" "$slice_suffix" && is_prompt_empty=0
-
-	# section "jobsnum" header
-	slice_prefix="${warn_bg}${sep}${warn_fg}${warn_bg}${space}"
-	slice_suffix="$space${warn_sep_fg}"
-	slice_empty_prefix="${warn_fg}${warn_bg}${space}"
-	[ $is_prompt_empty -eq 1 ] && slice_prefix="$slice_empty_prefix"
-	# section "jobsnum" slices
-	__promptline_wrapper "$(jobsnum)" "$slice_prefix" "$slice_suffix" && is_prompt_empty=0
-
-	# close sections
-	echo -n "${reset_bg}${sep}$reset$space"
-}
-
-function __promptline_wrapper {
-	# wrap the text in $1 with $2 and $3, only if $1 is not empty
-	# $2 and $3 typically contain non-content-text, like color escape codes and separators
-
-	[[ -n "$1" ]] || return 1
-	echo -n "${2}${1}${3}"
+	[ "$branch" ] || branch=$sha
+	let ps1_len+=${#branch}+5
+	ps1="$ps1${1} ${branch}${2}"
 }
 
 function __promptline {
@@ -101,7 +64,36 @@ function __promptline {
 	local y_fg='\[\e[38;5;250m\]'
 	local y_bg='\[\e[48;5;236m\]'
 	local y_sep_fg='\[\e[38;5;236m\]'
-	PS1="$(__promptline_ps1)"
+	local ps1 ps1_len=0
+	local slice_prefix slice_empty_prefix slice_suffix is_prompt_empty=1
+
+	# section "c" header
+	slice_prefix="${c_bg}${sep}${c_fg}${c_bg}${space}"
+	slice_suffix="$space${c_sep_fg}"
+	slice_empty_prefix="${c_fg}${c_bg}${space}"
+	[ $is_prompt_empty -eq 1 ] && slice_prefix="$slice_empty_prefix"
+	# section "c" slices
+	__promptline_cwd "$slice_prefix" "$slice_suffix" && is_prompt_empty=0
+
+	# section "y" header
+	slice_prefix="${y_bg}${sep}${y_fg}${y_bg}${space}"
+	slice_suffix="$space${y_sep_fg}"
+	slice_empty_prefix="${y_fg}${y_bg}${space}"
+	[ $is_prompt_empty -eq 1 ] && slice_prefix="$slice_empty_prefix"
+	# section "y" slices
+	git_branch "$slice_prefix" "$slice_suffix" && is_prompt_empty=0
+
+	# section "jobsnum" header
+	slice_prefix="${warn_bg}${sep}${warn_fg}${warn_bg}${space}"
+	slice_suffix="$space${warn_sep_fg}"
+	slice_empty_prefix="${warn_fg}${warn_bg}${space}"
+	[ $is_prompt_empty -eq 1 ] && slice_prefix="$slice_empty_prefix"
+	# section "jobsnum" slices
+	jobsnum "$slice_prefix" "$slice_suffix" && is_prompt_empty=0
+
+	# close sections
+	ps1="$ps1${reset_bg}${sep}$reset$space"
+	[[ $ps1_len -lt $COLUMNS ]] && PS1="$ps1" || PS1='\[\e[0;31m\]⑆ \[\e[0m\]'
 }
 
 # PS1="\[\033[01;35m\]\w\[\033[01;32m\]\$(git-branch)\[\033[01;31m\]\$(jobsnum)\[\033[01;33m\] » \[\033[00m\]"
