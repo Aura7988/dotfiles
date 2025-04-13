296866351243dc2bd77d9c717631f423491cce37
Aura <aura8897@gmail.com>
2025-04-13 20:46:27 +0800

    update updateneovim.sh

--- scripts/updateneovim.sh
+++ scripts/updateneovim.sh
@@ -1,32 +1,29 @@
-#!/bin/bash
+#!/bin/bash -e
 
 cd ~/softwares
 
 # oldfile=nvim$(date +%Y%m%d%H%M%S).tgz
 oldfile=oldnvim.tgz
+[[ $# -eq 0 ]] && newfile=nvim-macos-arm64.tar.gz || newfile=nvim-linux-x86_64.tar.gz
 
-trap "mv $oldfile nvim-linux64.tar.gz; exit 1" SIGINT
-
-mv nvim-linux64.tar.gz $oldfile
-curl -OL https://github.com/neovim/neovim/releases/download/nightly/nvim-linux64.tar.gz || { mv $oldfile nvim-linux64.tar.gz; exit 1; }
+trap "mv $oldfile $newfile; exit 1" SIGINT
+[[ -f $newfile ]] && mv $newfile $oldfile
+curl -OL https://github.com/neovim/neovim/releases/download/nightly/$newfile || { mv $oldfile $newfile; exit 1; }
 
 oldmd5=`md5sum $oldfile | cut -c-32`
-newmd5=`md5sum nvim-linux64.tar.gz | cut -c-32`
+newmd5=`md5sum $newfile | cut -c-32`
+[[ "$oldmd5" == "$newmd5" ]] && { echo 'no updates'; exit; }
 
-# echo $oldmd5
-# echo $newmd5
-if [[ "$oldmd5" != "$newmd5" ]]; then
-	# fuser ~/.local/bin/nvim &> /dev/null && echo 'nvim busy' && exit
-	# pgrep nvim && echo 'nvim busy' && exit
-	tar -xf nvim-linux64.tar.gz
-	cd nvim-linux64
-	# patchelf --set-rpath ~/.local/glibc236/lib bin/nvim
-	# patchelf --set-interpreter ~/.local/glibc236/lib/ld-linux-x86-64.so.2 bin/nvim
-	rm -rf ~/.local/share/nvim ~/.local/lib/nvim
-	until cp -r * ~/.local; do
-		echo 'nvim busy'
-		sleep 1
-	done
-	rm -rf ../nvim-linux64
-	echo 'new version installed'
-fi
+# fuser ~/.local/bin/nvim &> /dev/null && echo 'nvim busy' && exit
+# pgrep nvim && echo 'nvim busy' && exit
+tar -xf $newfile
+dir=${newfile%%.*}
+# patchelf --set-rpath ~/.local/glibc236/lib bin/nvim
+# patchelf --set-interpreter ~/.local/glibc236/lib/ld-linux-x86-64.so.2 bin/nvim
+rm -rf ~/.local/share/nvim ~/.local/lib/nvim
+until cp -r $dir/* ~/.local; do
+	echo 'nvim busy'
+	sleep 1
+done
+rm -rf $dir
+echo 'new version installed'
